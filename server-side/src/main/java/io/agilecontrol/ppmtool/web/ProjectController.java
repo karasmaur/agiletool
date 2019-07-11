8609c8487a1e5e7dc697438f2bf83285711e0d78
mkaras <mkaras@atlassian.com>
2019-07-11 18:19:06 -0300

    Refactoring the error mapping.

--- server-side/src/main/java/io/agilecontrol/ppmtool/web/ProjectController.java
+++ server-side/src/main/java/io/agilecontrol/ppmtool/web/ProjectController.java
@@ -1,6 +1,7 @@
 package io.agilecontrol.ppmtool.web;
 
 import io.agilecontrol.ppmtool.domain.Project;
+import io.agilecontrol.ppmtool.services.MapValidationErrorService;
 import io.agilecontrol.ppmtool.services.ProjectService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -23,18 +24,14 @@ public class ProjectController {
     @Autowired
     private ProjectService projectService;
 
+    @Autowired
+    private MapValidationErrorService mapValidationErrorService;
+
     @PostMapping("")
     public ResponseEntity<?> createNewProject(@Valid @RequestBody Project project, BindingResult result){
 
-        if(result.hasErrors()){
-            Map<String, String> errorMap = new HashMap<>();
-
-            for (FieldError error: result.getFieldErrors()) {
-                errorMap.put(error.getField(), error.getDefaultMessage());
-            }
-
-            return new ResponseEntity<Map<String, String>>(errorMap, HttpStatus.BAD_REQUEST);
-        }
+        ResponseEntity<?> errorMap = mapValidationErrorService.MapValidationErrors(result);
+        if(errorMap != null) return errorMap;
 
         Project returnProject = projectService.saveOrUpdateProject(project);
 
